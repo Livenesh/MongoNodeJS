43627d72a9350c14eb3f8daf368c2b4490d49b68
Livenesh <81608508+Livenesh@users.noreply.github.com>
2021-07-13 01:14:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ async function main(){
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.7sjg7.mongodb.net/sample_airbnb";
+    const uri = "mongodb+srv://<username>:<password>@sandbox.7sjg7.mongodb.net/sample_airbnb";
  
 
     const client = new MongoClient(uri);
